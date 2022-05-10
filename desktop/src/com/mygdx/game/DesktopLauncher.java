0d0a574a67f2cd3d6b95b33df681909218306fc9
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2022-05-10 12:36:51 +0600

    Game Name Fixed

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
+		config.setTitle("Survive It");
 		config.setWindowedMode(MyGdxGame.SCREEN_WIDTH, MyGdxGame.SCREEN_HEIGHT);
 		config.setResizable(false);
 		new Lwjgl3Application(new MyGdxGame(), config);
